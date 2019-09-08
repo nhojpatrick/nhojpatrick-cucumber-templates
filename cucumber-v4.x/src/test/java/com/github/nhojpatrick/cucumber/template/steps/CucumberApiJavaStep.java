bb6cd85a554a22ff660d56f25ee30b5429d3de13
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2019-09-08 12:34:13 +0100

    Both packages CucumberApi and IoCucumber

--- cucumber-v4.x/src/test/java/com/github/nhojpatrick/cucumber/template/steps/CucumberApiJavaStep.java
+++ cucumber-v4.x/src/test/java/com/github/nhojpatrick/cucumber/template/steps/CucumberApiJavaStep.java
@@ -8,26 +8,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-public class CucumberStep implements En {
+public class CucumberApiJavaStep implements En {
 
-    public CucumberStep() {
-        And("Background lambda step name {string} matches value {string}",
+    public CucumberApiJavaStep() {
+        And("CucumberApiJava Background lambda step name {string} matches value {string}",
                 (String name, String value) -> nameNameMatchesValueValue(name, value));
     }
 
-    @Given("Background step takes ages")
+    @Given("CucumberApiJava Background step takes ages")
     public void backgroundStepTakesAges()
             throws Exception {
         Thread.sleep(400);
     }
 
-    @Given("Scenario Outline step takes ages")
+    @Given("CucumberApiJava Scenario Outline step takes ages")
     public void scenarioOutlineStepTakesAges()
             throws Exception {
         Thread.sleep(550);
     }
 
-    @Then("name {string} matches value {string}")
+    @Then("CucumberApiJava name {string} matches value {string}")
     public void nameNameMatchesValueValue(String name, String value) {
         assertThat(name, is(equalTo(value)));
     }
